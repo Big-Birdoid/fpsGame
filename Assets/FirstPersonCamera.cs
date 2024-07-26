10df827dae3ac35e4181bd833a490fb4b2d5dd70
Big-Birdoid <153837778+Big-Birdoid@users.noreply.github.com>
2024-07-26 11:45:18 +0100

    Changed my mind on the necessity of the input manager

--- Assets/FirstPersonCamera.cs
+++ Assets/FirstPersonCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class inputManager : MonoBehaviour
+public class FirstPersonCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
