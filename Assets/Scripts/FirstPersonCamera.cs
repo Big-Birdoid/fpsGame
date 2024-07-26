3166d0d345a09f16bf41659830d20382441fe21d
Big-Birdoid <153837778+Big-Birdoid@users.noreply.github.com>
2024-07-26 12:59:24 +0100

    Reapply "moved some files"
    This reverts commit b8f63d8faf401fc9c4ac9bb6a8a78a93e84009ba.

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+
+    // Set up the variables needed
+        public float sensX = 100.0f;
+        public float sensY = 100.0f;
+
+        public Transform orientation;
+
+        float xRotation;
+        float yRotation;
+
+    void Start()
+    {
+        // lock the cursor in the center of the screen and make it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensX * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY * Time.deltaTime;
+
+        // Adjust rotations
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+       // Clamp rotation to avoid flipping
+       xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate the camera
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+}
