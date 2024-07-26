cc54597be50c70d2de181fbc6c8b1cfed14148b2
Big-Birdoid <153837778+Big-Birdoid@users.noreply.github.com>
2024-07-26 15:26:33 +0100

    Movement functionality added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,43 @@
-
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Initialises the necessary variables.
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        InputDirection();
+        MovePlayer();
     }
+
+    void InputDirection()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //Calculate move direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+    }
+
+    void MovePlayer()
+    {
+        // adds force to the player in the direction of the movement input
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f * Time.deltaTime, ForceMode.Force);
+    }
+
 }
