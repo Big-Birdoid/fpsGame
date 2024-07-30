92d719b2e2313640e3571bd24f6234356bf9a6e4
Big-Birdoid <153837778+Big-Birdoid@users.noreply.github.com>
2024-07-30 13:44:17 +0100

    Drag added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-
     // Initialises the necessary variables.
-    public float moveSpeed;
+    public float moveSpeed; // public so i can adjust it in the inspector.
+    public float groundDrag; // The drag applied to the player when they are on the ground.
 
-    public Transform orientation;
+    [Header("Ground Detection")]
+    public float playerHeight; // Used for the length of the raycast that detects the ground.
+    public LayerMask whatIsGround; // Needed to know what is considered ground.
+    bool grounded; // Flags if the player is on the ground.
 
+    [Header("Transforms")]
+    public Transform orientation; // Used to determine the direction of the player's movement.
+
+    // wasd input
     float horizontalInput;
     float verticalInput;
 
-    Vector3 moveDirection;
+    Vector3 moveDirection; // final move direction
 
-    Rigidbody rb;
+    Rigidbody rb; // physics
 
     void Start()
     {
@@ -25,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        // Do movement stuff
         InputDirection();
         MovePlayer();
     }
@@ -43,6 +52,27 @@ public class PlayerMovement : MonoBehaviour
     {
         // adds force to the player in the direction of the movement input
         rb.AddForce(moveDirection.normalized * moveSpeed * 100f * Time.deltaTime, ForceMode.Force); // Changed the multiplier to 100f so that the player actually moves.
+        ApplyDrag();
+    }
+
+    void ApplyDrag() // Calculates the drag to be applied based on the player's velocity. (totally realistic physics going on here!)
+    {
+        // Check for ground with a raycast    
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        // handle drag
+        if (grounded && moveDirection.magnitude == 0 && rb.velocity.magnitude != 0) // If the player is on the ground, not pressing a movement key, and still has velocity.
+        {
+            rb.drag = groundDrag; // Apply the ground drag.
+        }
+        else if (grounded && moveDirection.magnitude != 0) // If the player is on the ground and pressing a movement key.
+        {
+            rb.drag = groundDrag * 0.1f; // Apply the ground drag.
+        }
+        else
+        {
+            rb.drag = 0f; // if the player is not on the ground, do no drag
+        }
     }
 
 }
