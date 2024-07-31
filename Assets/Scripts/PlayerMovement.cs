103ca7ced9c6aca09552c1074ec0c4932a0fc89c
Big-Birdoid <153837778+Big-Birdoid@users.noreply.github.com>
2024-07-31 09:37:16 +0100

    jumping added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,15 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     // Initialises the necessary variables.
-    public float moveSpeed; // public so i can adjust it in the inspector.
-    public float groundDrag; // The drag applied to the player when they are on the ground.
+    public float moveSpeed; // max speed
+    public float groundDrag; // base ground friction
+    public float jumpForce; // The force applied to the player when they jump.
+    public float jumpCooldown; // time between jumps
+    public float airMultiplier; // how much faster or slower the player moves in the air
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
 
     [Header("Ground Detection")]
     public float playerHeight; // Used for the length of the raycast that detects the ground.
@@ -28,31 +35,48 @@ public class PlayerMovement : MonoBehaviour
         // Gets the rigidbody component of the player.
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        readyToJump = true;
     }
 
     void Update()
     {
         // Do movement stuff
-        InputDirection();
+        PlayerInput();
         MovePlayer();
         SpeedControl();
     }
 
-    void InputDirection() // Determines the direction of the player's movement.
+    void PlayerInput() // Handles stuff to do with player input.
     {
         // Gets the input from the player.
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        //Calculate move direction
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        // Jumping
+        if (Input.GetKeyDown(jumpKey) && grounded && readyToJump)
+        {
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
-    void MovePlayer()
+    void MovePlayer() // Handles the movement of the player.
     {
+        //Calculate move direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
         // adds force to the player in the direction of the movement input
-        rb.AddForce(moveDirection.normalized * moveSpeed * 100f * Time.deltaTime, ForceMode.Force); // Changed the multiplier to 100f so that the player actually moves.
-        ApplyDrag();
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 100f * Time.deltaTime, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 100f * airMultiplier * Time.deltaTime, ForceMode.Force);
+        }
+        ApplyDrag(); // Applies the appropriate drag
+
+        // Also I am aware that I'm checking if the player is grounded twice. If performance becomes a problem I will fix it later.
     }
 
     void ApplyDrag() // Calculates the drag to be applied based on the player's velocity. (totally realistic physics going on here!)
@@ -81,6 +105,21 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = rb.velocity.normalized * moveSpeed; // Set the player's velocity to the move speed.
         }
+    }
+
+    void Jump()
+    {
+        readyToJump = false;
+
+        // reset y velocity to ensure consistent jump height
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); // Apply the jump force.
+    }
+
+    void ResetJump()
+    {
+        readyToJump = true;
     }
 
 }
